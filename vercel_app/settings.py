fe2b7f9e3315dd04ccf45e1a25d5903647345d8e
muhamad.zolfaghari <you@example.com>
2021-09-24 18:22:29 +0330

    The second test for deploy.

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'vercel_app.urls'
+ROOT_URLCONF = 'finalsite.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'vercel_app.wsgi.application'
+WSGI_APPLICATION = 'finalsite.wsgi.application'
 
 
 # Database
