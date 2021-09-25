27ec85cd028cd2a1ce69a88837328c54131afa13
muhamad.zolfaghari <you@example.com>
2021-09-25 22:10:26 +0330

    Some file of project was updated.

--- loginapp/views.py
+++ loginapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
-from rest_auth.registration.views import SocialLoginView
+# from rest_auth.registration.views import SocialLoginView
 
 # Create your views here.
 
--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'example',
     'api',
+    'loginapp'
 ]
 
 MIDDLEWARE = [
