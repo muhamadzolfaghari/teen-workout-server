ac049f23a5534e73e1d2415d960efd2947c4a81d
raufmasoumi <raouf.rm93@gmail.com>
2021-09-26 21:02:52 +0330

    loginapp urls.py was changed.

--- loginapp/urls.py
+++ loginapp/urls.py
@@ -2,5 +2,5 @@ from django.http import JsonResponse
 from django.urls import path
 
 urlpatterns = [
-    path('auth/google/', lambda request: JsonResponse({'value': 12451545}), name='hello_world')
+    path('auth/google/', lambda request: JsonResponse({'value': 'This is new update'}), name='hello_world')
 ]
