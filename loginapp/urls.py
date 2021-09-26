05598488b06047e2fa2e914e78261fe2d1a2204d
raufmasoumi <raouf.rm93@gmail.com>
2021-09-26 21:11:16 +0330

    Revert "Revert "urls.py was updated from raof mac.""
    This reverts commit 77f67f09

--- loginapp/urls.py
+++ loginapp/urls.py
@@ -1,1 +1,6 @@
-hiyhiu
+from django.http import JsonResponse
+from django.urls import path
+
+urlpatterns = [
+    path('auth/google/', lambda request: JsonResponse({'value': 'This is new update'}), name='hello_world')
+]
