58158f6717dca20c8cde00068468132c8c7a1b3a
Muhamad Zolfaghari <muhamad.zolfaghari@gmail.com>
2021-09-26 19:21:25 +0330

    Revert "Update urls.py"
    This reverts commit a3a6183abf9a5abd80b1124644c19772506fe682.

--- loginapp/urls.py
+++ loginapp/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path, include
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LogoutView
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 from .views import GoogleLogin
 
 urlpatterns = [
-  #path('auth/google/', lambda request: HttpResponse('Hello World!'), name='hello_world'),
-  path('auth/google/', lambda request: JsonResponse({'value':'Hello World!'}), name='hello_world')
-
+  path('auth/google/', lambda request: HttpResponse('Hello World!'), name='hello_world')
 ]
