1fda675dd7bd29f5deb3797a1e4316145fdb7e38
RaufMasoumi <87221625+RaufMasoumi@users.noreply.github.com>
2021-09-26 18:11:37 +0330

    Update urls.py

--- loginapp/urls.py
+++ loginapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LogoutView
+from django.http import HttpResponse
 from .views import GoogleLogin
 
-
-urlpatterns = []
+urlpatterns = [
+  path('auth/google/', lambda request: HttpResponse('Hello World!'), name='hello_world')
+]
