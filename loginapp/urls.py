2b3c8b2ce28ed88072186f8133add05ef702e961
raufmasoumi <raouf.rm93@gmail.com>
2021-09-26 21:08:18 +0330

    test

--- loginapp/urls.py
+++ loginapp/urls.py
@@ -1,6 +1,1 @@
-from django.http import JsonResponse
-from django.urls import path
-
-urlpatterns = [
-    path('auth/google/', lambda request: JsonResponse({'value': 'Hello World!'}), name='hello_world')
-]
+hiyhiu
