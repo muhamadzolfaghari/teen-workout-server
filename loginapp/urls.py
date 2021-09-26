924157698f3fb83cb6abcc972012f5fce74a0f67
Muhamad Zolfaghari <muhamad.zolfaghari@gmail.com>
2021-09-26 19:40:01 +0330

    HttpResponse was added.

--- loginapp/urls.py
+++ loginapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 # from django.views.generic import TemplateView
 # from django.contrib.auth.views import LogoutView
-# from django.http import HttpResponse
+from django.http import HttpResponse
 # from .views import GoogleLogin
 
 urlpatterns = [
