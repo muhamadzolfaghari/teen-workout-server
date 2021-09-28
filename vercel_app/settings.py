7b7eedf7ce17515003e43e4ff314b09585928165
raufmasoumi <raouf.rm93@gmail.com>
2021-09-28 12:07:06 +0330

    The conflict was resolved and merged.

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -30,6 +31,7 @@ ALLOWED_HOSTS = [
     '.vercel.app'
 ]
 
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -42,18 +44,19 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'rest_framework',
 
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
+
     'rest_auth',
     'rest_auth.registration',
-    #our applications
     'loginapp',
-    'data.apps.DataConfig',
-
-    # cors applications
-    'corsheaders',
-
+    'data.apps.DataConfig'
 ]
 
 MIDDLEWARE = [
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -87,6 +90,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'vercel_app.wsgi.application'
 
+
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
@@ -96,6 +100,7 @@ DATABASES = {
         'NAME': 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -115,6 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
@@ -128,10 +134,12 @@ USE_L10N = True
 
 USE_TZ = True
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
 
 GOOGLE_OAUTH2_CLIENT_ID = '213873025360-dk75b6uhmd2h67mfdphg3usn8bktcer6.apps.googleusercontent.com'
 
@@ -142,4 +150,3 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3001",
     "https://teen-workout-server-alpha.vercel.app"
 ]
-
