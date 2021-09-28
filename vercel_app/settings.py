5e212c34515759c1410d767cbe183db93c0914fb
Muhamad Zolfaghari <muhamad.zolfaghari@gmail.com>
2021-09-28 12:17:00 +0330

    The django-cors-headers package added to requirements.txt.

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -104,6 +104,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
+# https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
