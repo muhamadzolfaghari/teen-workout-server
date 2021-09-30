24ba4575e9c2f21c06854ee36fbe3def21affb97
raufmasoumi <raouf.rm93@gmail.com>
2021-09-30 20:43:53 +0330

    models.py updated and now the tables is ok.

--- data/models.py
+++ data/models.py
@@ -6,11 +6,17 @@ class AgeRange(models.Model):
     id = models.IntegerField(primary_key=True)
     ages = models.CharField(max_length=100)
 
+    class Meta:
+        db_table = 'age_range'
+
     def __str__(self):
         return self.ages
 
 class Gender(models.Model):
     genders = models.CharField(max_length=30)
+
+    class Meta:
+        db_table = 'gender'
 
     def __str__(self):
         return self.genders
@@ -22,6 +28,9 @@ class Workout(models.Model):
     repeat = models.IntegerField(null=True)
     description = models.TextField()
     age_range_id = models.ManyToManyField(AgeRange, null=True)
+
+    class Meta:
+        db_table = 'workout'
 
     def __str__(self):
         return self.workout
@@ -35,6 +44,9 @@ class User(models.Model):
     weight = models.IntegerField(null=True)
     gender_id = models.ForeignKey(Gender, on_delete=models.CASCADE, null=True, verbose_name='gender')
     age_range_id = models.ForeignKey(AgeRange, on_delete=models.CASCADE, null=True, verbose_name='age')
+
+    class Meta:
+        db_table = 'user'
 
     def __str__(self):
         return self.name
