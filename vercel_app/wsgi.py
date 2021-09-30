d7ca02d6eec889880d152bc8df0c10504365236b
Muhamad Zolfaghari <muhamad.zolfaghari@gmail.com>
2021-09-30 20:11:42 +0330

    Mysql was running successfully.

--- vercel_app/wsgi.py
+++ vercel_app/wsgi.py
@@ -9,7 +9,10 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 
+import pymysql
 from django.core.wsgi import get_wsgi_application
+
+pymysql.install_as_MySQLdb()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vercel_app.settings')
 
