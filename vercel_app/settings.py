43705f1259f040d9f5fc2856b23c088bf1458210
Muhamad Zolfaghari <muhamad.zolfaghari@gmail.com>
2021-10-01 20:01:30 +0330

    The settings.py was updated.

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -30,7 +29,6 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     '.vercel.app'
 ]
-
 
 # Application definition
 
@@ -47,7 +45,7 @@ INSTALLED_APPS = [
     'rest_auth',
     'rest_auth.registration',
 
-    #our applications
+    # our applications
     'loginapp',
     'data.apps.DataConfig',
 
@@ -91,7 +89,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'vercel_app.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
@@ -105,8 +102,6 @@ DATABASES = {
         'PORT': '3306',
     }
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -127,7 +122,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
@@ -140,7 +134,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
@@ -156,5 +149,5 @@ GOOGLE_OAUTH2_CLIENT_SECRET = 'm9jTPmdH_XpD8lR-jmHkebVC'
 # Trusted origins
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://teen-workout-server-alpha.vercel.app"
+    "https://teen-workout.vercel.app/"
 ]
