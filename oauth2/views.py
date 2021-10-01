aa001aff836bcb466446e906c62e76f226b66060
Muhamad Zolfaghari <muhamad.zolfaghari@gmail.com>
2021-10-01 13:12:09 +0330

    Mysql was running successfully.

--- oauth2/views.py
+++ oauth2/views.py
@@ -1,7 +1,10 @@
+import typing
+
 from django.http import JsonResponse
 
-from data.models import AgeRanges, Accounts
+from data.models import AgeRanges
 from oauth2 import utils
+from django.core import serializers
 
 
 class OAuth2:
@@ -9,10 +12,15 @@ class OAuth2:
         account_info = utils.get_accounts_info(access_token)
         # Accounts(account_info).save()
 
+        age_ranges_rows = AgeRanges.objects.all()
+
+        print(age_ranges_rows)
 
 
-        if account_info:
-            return JsonResponse(account_info)
-        else:
-            JsonResponse.status_code = 401
-            return JsonResponse({})
+        return JsonResponse(age_ranges)
+
+        # if account_info:
+        #     return JsonResponse(account_info)
+        # else:
+        #     JsonResponse.status_code = 401
+        #     return JsonResponse({})
