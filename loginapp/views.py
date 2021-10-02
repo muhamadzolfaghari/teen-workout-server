2893f6a39eb46f1bade91da0bf8625b1759abd52
Muhamad Zolfaghari <muhamad.zolfaghari@gmail.com>
2021-10-02 21:37:10 +0330

    this is committed from my phone😂

--- loginapp/views.py
+++ loginapp/views.py
@@ -1,25 +1,28 @@
-import os.path
-
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
-from django.views.decorators.csrf import requires_csrf_token, ensure_csrf_cookie, csrf_exempt, csrf_protect
-from django.views.decorators.http import require_http_methods, require_POST
+from django.views.decorators.csrf import requires_csrf_token, ensure_csrf_cookie
+from django.views.decorators.http import require_POST
+
 from data.models import AgeRanges, Genders, AccountsProfiles, Accounts
 from oauth2.utils import verify_access_token
 
 
 @requires_csrf_token
 def get_csrf(request):
-    return render(request,  'loginapp/index.html')
+    return render(request, 'loginapp/index.html')
 
 
 def metadata(void):
     return JsonResponse({})
     # return JsonResponse({"age_ranges": age_ranges, 'genders': genders})
 
-@csrf_exempt
+
+# @csrf_exempt
+# @ensure_csrf_cookie
 @require_POST
 def store_account_profile(wsgi: dict):
+    print('is worked properly!')
+
     json = wsgi.json()
     if verify_access_token(json['access_token']):
         account = Accounts.objects.filter(id=json['user_id'])
